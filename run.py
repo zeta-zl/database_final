e3a0891ee19494a823ef37f412fb7ea57c7dcc37
YingyuShan <shan030209@163.com>
2023-06-19 02:06:58 +0800

    test22

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
 import login
 
 if __name__ == '__main__':
-    for_test = 3
+    for_test = 3333333
     QApp = QApplication(sys.argv)
     login = login.loginDialog()
     login.show()
