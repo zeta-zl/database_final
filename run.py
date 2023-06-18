6f8e5e07b4a1a070bd85d4ad3382d35718c17886
Zeta <2267172585@qq.com>
2023-06-19 01:17:05 +0800

    编写了query_abstract作为默认的查询界面，query_medicine为一个示例 目前入口：主界面-医生登录，默认表格为medicine 具体说明在query_abstract文件中

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 import sys
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow,QDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
 
 import login
 
 if __name__ == '__main__':
-    for_test=1
+    for_test = 1
     QApp = QApplication(sys.argv)
     login = login.loginDialog()
     login.show()
