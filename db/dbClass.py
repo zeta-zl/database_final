92d9446c0c99c0c1da816cedf8f9471b5354c3c8
Zeta <2267172585@qq.com>
2023-06-28 18:40:59 +0800

    更新了初始化指引

--- db/dbClass.py
+++ db/dbClass.py
@@ -61,9 +61,11 @@ class dbClass():
 
     def connectdb(self):
         try:
+            passwd = ""
+            assert passwd != "", "需要填写本地数据库密码"
             self.conn = pymysql.connect(host='127.0.0.1'  # 连接名称，默认127.0.0.1
                                         , user='root'  # 用户名
-                                        , passwd='zeta'  # 密码
+                                        , passwd=passwd  # 密码
                                         , port=3306  # 端口，默认为3306
                                         , db='hospital'  # 数据库名称
                                         , charset='utf8'  # 字符编码
